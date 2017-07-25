609e745acd2eedfffc5e158cd6c89c8966a2f75a
Stephanos Theodotou <stephanostheodotou@Stephanoss-MacBook-Pro.local>
2017-07-25 21:38:55 +0200

    add data file, add directive to loop through data source and render the beverage kind as a section

--- src/app/components/drinks/drinks.component.ts
+++ src/app/components/drinks/drinks.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import drinksList from "./drinks";
 
 @Component({
   selector: 'app-drinks',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./drinks.component.css']
 })
 export class DrinksComponent implements OnInit {
+  drinksList: Object[];
 
   constructor() { }
 
---
+++ src/app/components/drinks/drinks.ts
@@ -0,0 +1,20 @@
+const drinksList: object[] = [
+{
+  kind: "Whisky",
+  cocktails: [{
+                name: "Old Fashioned",
+                Volume: 40,
+                Ingredients: ["one", "two", "three"]
+  }]
+},
+{
+  kind: "Vodka",
+  cocktails: [{
+                name: "Martini",
+                Volume: 28,
+                Ingredients: ["four", "five", "six"]
+  }]
+}
+];
+
+export default drinksList;
